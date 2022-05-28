44bccb8bc2daa5017844d2d61a7198df02c79617
Boakai <boakaidkamara@gmail.com>
2022-05-28 18:45:06 +0000

    added attribute for Student class

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ class Student:
     # [assignment] Skeleton class. Add your code here
     def __init__(self):
         pass
+        Name = "Boakai Dorbor Kamara"
+        Age = 15
+        Tracks = [General,FullStack]
+        Score = 9.524
 
+
+Name: A string, should be initialized when creating an instance of Student
+ Age: An integer, should be initialized when creating an instance of Student
+Tracks: A list of strings, should be initialized when creating an instance of Student. Feel free to pick any values as tracks.
+ Score: A float, should be initialized when creating an instance of Student.  
 
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
