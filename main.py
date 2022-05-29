9caa2fe6556c43a6bdc08b57145c9ea0567ab9e9
Boakai <boakaidkamara@gmail.com>
2022-05-29 02:24:03 +0000

    fix bug while calling attributes

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
+    def __init__(self,name,age,tracks,score):
         pass
-        Name = "Boakai Dorbor Kamara"
-        Age = 15
-        Tracks = [General,FullStack]
-        Score = 9.524
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
 
 
-Name: A string, should be initialized when creating an instance of Student
- Age: An integer, should be initialized when creating an instance of Student
-Tracks: A list of strings, should be initialized when creating an instance of Student. Feel free to pick any values as tracks.
- Score: A float, should be initialized when creating an instance of Student.  
-
+        # def change_name(new_name):
+        #     Name = new_name
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
+print(Bob.name)
+print(Bob.age)
+print(Bob.tracks)
+print(Bob.score)
+
+
+
 # Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
+# Bob.change_name("Peter")
+# Bob.change_age(34)
+# Bob.add_track("UI/UX")
+# Bob.get_score()
